e214ca029774b3ce0303a7df30c327c1776c939c
uwe-group-c <uwegroupc@gmail.com>
2018-03-04 09:41:21 +0000

    First Heroku Upload

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express  = require('express');
 var app      = express();                               
-var morgan = require('morgan');            
+//var morgan = require('morgan');            
 var bodyParser = require('body-parser');    
 var cors = require('cors');
  
